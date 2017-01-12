cef238da3e0e9768cb24632af5bf5e1c497e3859
Konnsti <Konnsti-w@gmx.de>
2017-01-12 11:38:50 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("mysql10.000webhost.com", "id523780_login", "IhKW1993", "id523780_login");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
@@ -17,7 +17,6 @@
     while(mysqli_stmt_fetch($statement)){
         $response["success"] = true;  
         $response["name"] = $name;
-        $response["age"] = $age;
         $response["username"] = $username;
         $response["password"] = $password;
     }
